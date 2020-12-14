4d1a5962b82a28dddbeccf7986137c704597baef
rodrigorafaeldamaceno <rodrigorafaeldamaceno@gmail.com>
2020-12-14 15:08:38 -0300

    examples

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Sliver test',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
@@ -22,7 +23,92 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            title: Text('Sliver test'),
+            // floating: true,
+            // expandedHeight: 100,
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              height: 200,
+              child: ListView.builder(
+                itemCount: 10,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (BuildContext context, int index) {
+                  return Container(
+                    padding: EdgeInsets.only(right: 20),
+                    child: Column(
+                      children: <Widget>[
+                        // AspectRatio(
+                        //   aspectRatio: 3 / 4,
+                        //   child: Image.network(
+                        //     'https://avatars1.githubusercontent.com/u/15212018?s=400&u=6c7451e78f89c5bf44488bea8d6eb2daa40a28e0&v=4',
+                        //   ),
+                        // ),
+                        Image.network(
+                          'https://avatars1.githubusercontent.com/u/15212018?s=400&u=6c7451e78f89c5bf44488bea8d6eb2daa40a28e0&v=4',
+                          height: 180,
+                        ),
+                        Text(
+                          'Rodrigo Rafael',
+                        )
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return ListTile(
+                  leading: Icon(Icons.person),
+                  title: Text('List 1'),
+                );
+              },
+              childCount: 10,
+            ),
+          ),
+          SliverGrid.count(
+            crossAxisCount: 3,
+            childAspectRatio: 3 / 4,
+            crossAxisSpacing: 20,
+            mainAxisSpacing: 20,
+            children: List.generate(
+              20,
+              (index) => Image.network(
+                'https://avatars1.githubusercontent.com/u/15212018?s=400&u=6c7451e78f89c5bf44488bea8d6eb2daa40a28e0&v=4',
+                height: 180,
+              ),
+            ).toList(),
+          ),
+          SliverFixedExtentList(
+            itemExtent: 100,
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return ListTile(
+                  hoverColor: Colors.grey,
+                  title: Text('List 2'),
+                );
+              },
+              childCount: 10,
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return ListTile(
+                  title: Text('List 3'),
+                );
+              },
+              childCount: 10,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
